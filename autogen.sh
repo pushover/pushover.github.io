e1b0839edaa8f82ef3b38795d9b39a678bc7abca
Volker Grabsch <vog@notjusthosting.com>
2008-10-02 13:34:11 +0000

    simplified build system

--- autogen.sh
+++ autogen.sh
@@ -1,35 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
-echo "$0: running some preparations ..."
-(
-	version="`cat VERSION`"
-	extra_dist="`cd m4/ && ls -1 *.m4 | tr '\n' ' '`"
-	all_linguas="`echo -n "$linguas" | tr '\n' ' '`"
-
-	echo "Creating m4/Makefile.am"
-	echo "EXTRA_DIST = $extra_dist" >m4/Makefile.am
-
-	echo "Updating configure.ac"
-	sed \
-		-e 's/\(ALL_LINGUAS="\)[^"]*/\1'"$all_linguas"'/' \
-		-e 's/\(AC_INIT([^,]*,\)[^,]*\([,)].*\)/\1'"[$version]"'\2/' \
-		-i configure.ac
-)
-
-echo "$0: running aclocal ..."
-aclocal || exit 1
-
-echo "$0: running libtoolize ..."
-libtoolize --force || exit 1
-
-echo "$0: running autoconf ..."
-autoconf || exit 1
-
-echo "$0: running autoheader ..."
-autoheader || exit 1
-
-echo "$0: running automake ..."
-automake --add-missing || exit 1
-
-echo "$0: running configure ..."
-./configure $@ || exit 1
+set -e
+autoreconf -i -s
+./configure "$@"
