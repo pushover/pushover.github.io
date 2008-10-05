b60604112e5aa7cb8b93786480e2f6de484bc941
Volker Grabsch <vog@notjusthosting.com>
2008-10-05 14:35:15 +0000

    use C++ streams in recorder_c::load() and ::save(), fix a potential buffer overflow

--- src/recorder.cpp
+++ src/recorder.cpp
@@ -1,51 +1,43 @@
 #include "recorder.h"
 
-#include <stdio.h>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
 
 void recorder_c::load(const std::string & filename) {
-  FILE * f = fopen(filename.c_str(), "r");
+  std::ifstream stream(filename.c_str());
 
-  static char ll[200];
-  fgets(ll, 200, f);
-
-  // remove the newline at the end
-  ll[strlen(ll)-1] = 0;
-  levelName = ll;
+  if (!getline(stream, levelName))
+    throw std::exception();
 
   playpos = 0;
 
   record.clear();
-  while (!feof(f))
+  while (stream)
   {
     int cnt, val;
-    fscanf(f, "%i %i\n", &cnt, &val);
+    stream >> cnt;
+    stream >> val;
     for (int i = 0; i < cnt; i++)
       record.push_back(val);
   }
 }
 
 void recorder_c::save(void) const {
-  int num = 0;
-  FILE * f = 0;
+  /* find first unused filename */
+  std::string filename;
+  for (unsigned int num = 1; ; num++) {
+    std::ostringstream s;
+    s << "recordings/" << std::setfill('0') << std::setw(5) << num << ".rec";
+    filename = s.str();
+    std::ifstream stream(filename.c_str());
+    if (!stream)
+      break;
+  }
 
-  do {
-    num++;
-
-    if (f) fclose(f);
-
-    char fname[200];
-
-    snprintf(fname, 200, "recordings/%05i.rec", num);
-    f = fopen(fname, "r");
-
-  } while (f);
-
-  char fname[200];
-
-  snprintf(fname, 200, "recordings/%05i.rec", num);
-  f = fopen(fname, "w");
-
-  fprintf(f, "%s\n", levelName.c_str());
+  /* save to that filename */
+  std::ofstream stream(filename.c_str());
+  stream << levelName << '\n';
 
   int val = record[0];
   int cnt = 1;
@@ -54,7 +46,7 @@ void recorder_c::save(void) const {
   while (pos < record.size()) {
 
     if (record[pos] != val) {
-      fprintf(f, "%i %i\n", cnt, val);
+      stream << cnt << ' ' << val << '\n';
       val = record[pos];
       cnt = 1;
     }
@@ -64,9 +56,7 @@ void recorder_c::save(void) const {
     }
     pos++;
   }
-
-  fprintf(f, "%i %i\n", cnt, val);
-  fclose(f);
+  stream << cnt << ' ' << val << '\n';
 }
 
 
