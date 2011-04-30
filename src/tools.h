8e91de69129a619458c01e7cd1daad1d2f34ce38
Volker Grabsch <vog@notjusthosting.com>
2011-04-30 17:55:05 +0000

    improved comment

--- src/tools.h
+++ src/tools.h
@@ -5,7 +5,7 @@
 #include <vector>
 
 // return the path to the home directory
-// this is implemented in a platform dependent way
+// (implemented in a platform dependent way)
 std::string getHome(void);
 
 // return all non-hidden directory entries
