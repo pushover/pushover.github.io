0fb08caa0866846b2c72b2ef06e11beaaee86e90
Andreas Röver <roever@users.sourceforge.net>
2013-01-05 22:31:21 +0000

    allow unicode input for profiles

--- src/window.cpp
+++ src/window.cpp
@@ -815,6 +815,39 @@ InputWindow_c::InputWindow_c(int x, int y, int w, int h, surface_c & s, graphics
   redraw();
 }
 
+
+static size_t utf8EncodeOne(uint16_t uni, char *utf8, size_t bufspace)
+{
+  size_t bytes, si;
+
+  if (uni <= 0x7F) {
+    /* one byte, simple */
+    if (bufspace < 1) return 0;
+    utf8[0] = (char) uni;
+    return 1;
+
+  } else if (uni <= 0x7FF) { /* 0b110ddddd 10dddddd */
+    /* two bytes */
+    if (bufspace < 2) return 0;
+    bytes = 2;
+    utf8[0] = 0xC0 | (uni >> 6);
+
+  } else  { /* 0b1110dddd 10dddddd 10dddddd */
+    /* three bytes */
+    if (bufspace < 3) return 0;
+    bytes = 3;
+    utf8[0] = 0xE0 | ((uni >> 12) & 0x0F);
+
+  }
+
+  for (si = bytes - 1; si > 0; si--) {
+    utf8[si] = 0x80 | (uni & 0x3F);
+    uni >>= 6;
+  }
+
+  return bytes;
+}
+
 bool InputWindow_c::handleEvent(const SDL_Event & event)
 {
   if (window_c::handleEvent(event)) return true;
@@ -836,6 +869,12 @@ bool InputWindow_c::handleEvent(const SDL_Event & event)
     {
       if (cursorPosition > 0) {
         cursorPosition--;
+        while (   (cursorPosition > 0)
+               && ((input[cursorPosition] & 0xC0) == 0x80)
+              )
+        {
+          cursorPosition--;
+        }
         redraw();
       }
       return true;
@@ -844,6 +883,12 @@ bool InputWindow_c::handleEvent(const SDL_Event & event)
     {
       if (cursorPosition+1 <= input.length()) {
         cursorPosition++;
+        while (   (cursorPosition < input.length())
+               && ((input[cursorPosition] & 0xC0) == 0x80)
+              )
+        {
+          cursorPosition++;
+        }
         redraw();
       }
       return true;
@@ -866,28 +911,47 @@ bool InputWindow_c::handleEvent(const SDL_Event & event)
     }
     else if (event.key.keysym.sym == SDLK_BACKSPACE)
     {
+      while (   (cursorPosition > 0)
+             && ((input[cursorPosition-1] & 0xC0) == 0x80)
+            )
+      {
+        input.erase(cursorPosition-1, 1);
+        cursorPosition--;
+      }
       if (cursorPosition > 0)
       {
         input.erase(cursorPosition-1, 1);
         cursorPosition--;
-        redraw();
       }
+      redraw();
     }
     else if (event.key.keysym.sym == SDLK_DELETE)
     {
       if (cursorPosition < input.length())
       {
         input.erase(cursorPosition, 1);
+
+        while (   (cursorPosition < input.length())
+               && ((input[cursorPosition] & 0xC0) == 0x80)
+              )
+        {
+          input.erase(cursorPosition, 1);
+        }
         redraw();
       }
     }
-    else if (event.key.keysym.unicode >= 32 && event.key.keysym.unicode <= 127)
-      // sorry folks only latin characters
+    else if (event.key.keysym.unicode >= 32)
     {
       if (getTextWidth(FNT_NORMAL, input+(char)event.key.keysym.unicode) < gr.blockX()*(w-2))
       {
-        input.insert(cursorPosition, 1, (char)event.key.keysym.unicode);
-        cursorPosition++;
+        char utf8[10];
+        size_t s = utf8EncodeOne(event.key.keysym.unicode, utf8, 10);
+
+        for (int i = 0; i < s; i++)
+        {
+          input.insert(cursorPosition, 1, utf8[i]);
+          cursorPosition++;
+        }
         redraw();
       }
     }
@@ -927,8 +991,9 @@ void InputWindow_c::redraw(void)
   par.font = FNT_NORMAL;
   par.shadow = 0;
 
-  surf.fillRect(gr.blockX()*(x+1)+getTextWidth(FNT_NORMAL, input.substr(0, cursorPosition)),
-      ypos, 4, getFontHeight(FNT_NORMAL), 0, 0, 0);
+  unsigned int wi = getTextWidth(FNT_NORMAL, input.substr(0, cursorPosition));
+
+  surf.fillRect(gr.blockX()*(x+1)+wi, ypos, 4, getFontHeight(FNT_NORMAL), 0, 0, 0);
 
   par.box.y = ypos;
   surf.renderText(&par, input);
